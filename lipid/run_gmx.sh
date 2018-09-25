013f62aa148925233c2c556235a77fcb338d1b18
Justin MacCallum <justin.maccallum@ucalgary.ca>
2018-09-24 22:40:59 -0600

    Do gromacs analysis

--- lipid/run_gmx.sh
+++ lipid/run_gmx.sh
@@ -1,4 +1,6 @@
 cd gmx
 gmx grompp -f martini_md.mdp -c minimized.gro -p dppc.top -o run.tpr
 gmx mdrun -deffnm run -rerun minimized.gro
+echo pot | gmx energy -f run > energy.txt
+echo 0 | gmx traj -f run -s run -of forces
 cd ..
