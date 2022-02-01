2d9ead71617abd8bf080d8707d231ad940a48013
Justin MacCallum <justin.maccallum@ucalgary.ca>
2022-01-31 17:09:30 -0700

    Removed development system

--- tests/test.py
+++ tests/test.py
@@ -43,11 +43,6 @@ class TestBase:
         _cleanup()
 
 
-class TestPOPCDev(TestBase, unittest.TestCase):
-    test_dir = "popc_m3_dev14"
-    depth = 1
-
-
 class TestAdenine(TestBase, unittest.TestCase):
     test_dir = "aden_m3"
     depth = 1
